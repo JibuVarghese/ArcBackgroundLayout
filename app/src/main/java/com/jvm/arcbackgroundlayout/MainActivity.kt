f6e3992e438694fa120d65e5cd52af93e7f55000
Varghese <jibu@omnicuris.com>
2022-08-18 13:51:18 +0530

    set method added -- only works onCreate method

--- app/src/main/java/com/jvm/arcbackgroundlayout/MainActivity.kt
+++ app/src/main/java/com/jvm/arcbackgroundlayout/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<ArcBackgroundLayout>(R.id.arcLyt).setDrawArcPointPortrait(0.5f)
+        findViewById<ArcBackgroundLayout>(R.id.arcLyt).setCurveControlPointYPortrait(2.55f)
     }
 }
